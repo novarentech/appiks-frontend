75d41492b63ae0490b2014dee0f9d0b93a2c4fc7
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2026-07-23 19:28:11 +0700

    feat: add Accept: application/json header to API requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -126,6 +126,7 @@ export async function authenticatedFetch(
 
   const headers = {
     "Content-Type": "application/json",
+    Accept: "application/json",
     Authorization: `Bearer ${session.user.token}`,
     ...options.headers,
   };
